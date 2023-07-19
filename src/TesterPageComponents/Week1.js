def7431ccac7bac980bbbdacfaf81bcd1e36c0f1
David.McGregor <David.McGregor@hl.co.uk>
2023-07-19 15:42:20 +0100

    added people to meet arrays to week1 of the tester component

--- src/TesterPageComponents/Week1.js
+++ src/TesterPageComponents/Week1.js
@@ -1,6 +1,33 @@
 import React from "react";
 
 function Week1() {
+  const zeusSquatPeopleToMeet = [
+    "Zeus Squad Member 1",
+    "Zeus Squad Member 2",
+    "Zeus Squad Member 3",
+    "Zeus Squad Member 4",
+    "Zeus Squad Member 5",
+    "Zeus Squad Member 6",
+  ];
+
+  const prosperitySquatPeopleToMeet = [
+    "Prosperity Squad Member 1",
+    "Prosperity Squad Member 2",
+    "Prosperity Squad Member 3",
+    "Prosperity Squad Member 4",
+    "Prosperity Squad Member 5",
+    "Prosperity Squad Member 6",
+  ];
+
+  const automatesSquadPeopleToMeet = [
+    "Automates Squad Member 1",
+    "Automates Squad Member 2",
+    "Automates Squad Member 3",
+    "Automates Squad Member 4",
+    "Automates Squad Member 5",
+    "Automates Squad Member 6",
+  ];
+
   return (
     <div className="Week1">
       <h1>Week1</h1>
