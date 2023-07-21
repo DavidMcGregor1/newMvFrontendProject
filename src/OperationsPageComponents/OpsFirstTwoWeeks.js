8e7bf1e01e2947aa4548906d3c69a444eb3dc2ed
David.McGregor <David.McGregor@hl.co.uk>
2023-07-21 10:50:49 +0100

    declared ops first two weeks arrays

--- src/OperationsPageComponents/OpsFirstTwoWeeks.js
+++ src/OperationsPageComponents/OpsFirstTwoWeeks.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import "./OpsFirstTwoWeeks.css";
 
 function OpsFirstTwoWeeks() {
+  const [reflectionPointsCollection, setReflectionPointsCollection] = useState([
+    "",
+  ]);
+
+  const [opsGetUpToSpeedList, setOpsGetUpToSpeedList] = useState(["", "", ""]);
+
+  const [opsSavingsSpecificTasksList, opsSetSavingsSpecificTasksList] =
+    useState(["", "", ""]);
+
   return (
     <div className="OpsFirstTwoWeeks">
       <h1>OpsFirstTwoWeeks</h1>
