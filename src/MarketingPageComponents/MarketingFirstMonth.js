151bca5c4491773af103f37299b89254fb8a501d
David.McGregor <David.McGregor@hl.co.uk>
2023-07-25 08:42:34 +0100

    added marketing page components folder and created boiler plate files for first week, first two weeks and first month

---
+++ src/MarketingPageComponents/MarketingFirstMonth.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MarketingFirstMonth = () => {
+  return (
+    <div>
+      <h1>MarketingFirstMonth</h1>
+    </div>
+  );
+};
+
+export default MarketingFirstMonth;
