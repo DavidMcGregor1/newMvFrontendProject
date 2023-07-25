84beaa4652d50ac0481ff276e069b46774bd9cb5
David.McGregor <David.McGregor@hl.co.uk>
2023-07-25 11:00:43 +0100

    added test to check that developer page loads on click

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,3 +10,12 @@ describe("Main page loads and renders content", () => {
     cy.contains("Delivery Manager");
   });
 });
+
+describe("Developer page loads on click", () => {
+  it("should navigate to the Developer Page on Developer card click", () => {
+    cy.visit("localhost:3000/");
+    cy.contains("Developer").click();
+    cy.url().should("include", "/welcome/developer");
+    cy.contains("h1", "Week 1").should("be.visible");
+  });
+});
