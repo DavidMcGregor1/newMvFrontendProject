19c90589d8f9f1e2c45e99d6bc741613257b4e50
David.McGregor <David.McGregor@hl.co.uk>
2023-07-25 15:08:06 +0100

    added TesterPageSpec file and main content test

---
+++ cypress/e2e/TesterPageSpec.cy.js
@@ -0,0 +1,14 @@
+describe("Main page loads and renders content", () => {
+  it("Loads Week1 header", () => {
+    cy.visit("localhost:3000/welcome/tester");
+    cy.contains("h1", "Week 1").should("be.visible");
+  });
+  it("Loads Week 1 div sections", () => {
+    cy.visit("localhost:3000/welcome/tester");
+    cy.contains("h3", "Basic Set up").should("be.visible");
+    cy.contains("h3", "Get Compliant").should("be.visible");
+    cy.contains("h3", "People to meet").should("be.visible");
+    cy.contains("h3", "Resources").should("be.visible");
+    cy.contains("h3", "Add Task").should("be.visible");
+  });
+});
