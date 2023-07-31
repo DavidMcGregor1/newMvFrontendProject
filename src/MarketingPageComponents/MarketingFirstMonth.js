7132a6e680ad42e6f9e600404fe0509b889b9e46
David.McGregor <David.McGregor@hl.co.uk>
2023-07-31 16:01:05 +0100

    added marketing first month page

--- src/MarketingPageComponents/MarketingFirstMonth.js
+++ src/MarketingPageComponents/MarketingFirstMonth.js
@@ -1,9 +1,142 @@
 import React from "react";
+import "./MarketingFirstMonth.css";
+import { useState } from "react";
+import Button from "../UniversalComponents/Button";
+import ResourcesComponent from "../UniversalComponents/ResourcesComponent";
+import GeneralTasksSection from "../UniversalComponents/GeneralTasksSection";
 
 const MarketingFirstMonth = () => {
+  const [
+    firstMonthReflectionPointsCollection,
+    setfirstMonthReflectionPointsCollection,
+  ] = useState([""]);
+
+  const [generalTasks, setGeneralTasks] = useState([
+    "Keep getting involved in group discussions",
+    "Fill out reflection points on any areas that could be improved.",
+    "Set up regular catch ups with your manager and buddy",
+    "Have a think about what you want to spend your training budget on",
+    "Attend a social event!",
+  ]);
+
+  const [devFirstMonthAllResources, setDevFirstMonthAllResources] = useState([
+    {
+      text: "Village Halls",
+      link: "https://hlam-collab.atlassian.net/wiki/spaces/ID/pages/530057952/Village+halls",
+    },
+    {
+      text: "Active Savings Confluence Space",
+      link: "https://confluence.hargreaveslansdown.co.uk/display/AS/Active+Savings",
+    },
+    {
+      text: "Overview of Savings Architecture",
+      link: "fakeLink",
+    },
+    {
+      text: "Workday",
+      link: "https://wd3.myworkday.com/hargreaveslansdown/",
+    },
+    {
+      text: "Savings Cheat Sheet",
+      link: "https://hlam-collab.atlassian.net/wiki/spaces/AS/pages/494518362/Savings+Cheat+Sheet",
+    },
+  ]);
+
+  const [newTask, setNewTask] = React.useState("");
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== "") {
+      console.log("Inside if statement");
+      let updatedGeneralTasksList = [...generalTasks];
+      updatedGeneralTasksList.push(newTask);
+      setGeneralTasks(updatedGeneralTasksList);
+    }
+
+    setNewTask("");
+    console.log("Working");
+  };
+
+  const [newReflectionPoint, setNewReflectionPoint] = React.useState("");
+
+  const handleAddReflectionPoint = () => {
+    if (newReflectionPoint.trim() !== "") {
+      let updatedfirstMonthReflectionPointsCollection = [
+        ...firstMonthReflectionPointsCollection,
+      ];
+      updatedfirstMonthReflectionPointsCollection.push(newReflectionPoint);
+      console.log(
+        "First Month Reflection Collection:  -> " +
+          updatedfirstMonthReflectionPointsCollection
+      );
+      setfirstMonthReflectionPointsCollection(
+        updatedfirstMonthReflectionPointsCollection
+      );
+    }
+
+    setNewReflectionPoint("");
+    alert("Submitted Reflection Point");
+  };
+
   return (
-    <div>
-      <h1>MarketingFirstMonth</h1>
+    <div id="TesterFirstMonthMainContainer">
+      {/* ---------- GENERAL TASKS ----------  */}
+
+      <GeneralTasksSection generalTasks={generalTasks} />
+
+      {/* ---------- RESOURCES ----------  */}
+
+      <ResourcesComponent
+        resources={devFirstMonthAllResources}
+        varient="MarketingFirstMonthDevResources"
+        mainDiv="MarketingFirstMonthMainResourceDiv"
+        resourceItem="MarketingFirstMonthResourceItem"
+      />
+
+      {/* ---------- REFLECTION POINTS ----------  */}
+
+      <div className="TesterFirstMonthReflectionPoints">
+        <h3 className="sectionHeader" id="reflectionPointsHeader">
+          Reflection Points
+        </h3>
+
+        <div className="addReflectionPointInputDiv">
+          <textarea
+            id="FirstMonthAddReflectionPointInputBox"
+            type="text"
+            value={newReflectionPoint}
+            onChange={(e) => setNewReflectionPoint(e.target.value)}
+          />
+        </div>
+
+        <div className="FirstMonthSubmitReflectionButtonDiv">
+          <Button label="Submit" onClick={handleAddReflectionPoint} />
+        </div>
+      </div>
+
+      {/* ---------- ADD TASK ----------  */}
+
+      <div className="TesterFirstMonthAddTask">
+        <h3 className="sectionHeader" id="addTaskHeader">
+          Add Task
+        </h3>
+
+        <div className="addTaskInputDiv">
+          <input
+            id="devFirstMonthAddTaskInputBox"
+            type="input"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+        </div>
+
+        <div className="FirstMonthAddTaskButtonDiv">
+          <Button
+            id="FirstMonthAddTaskButton"
+            label="Add Task"
+            onClick={handleAddTask}
+          />
+        </div>
+      </div>
     </div>
   );
 };
