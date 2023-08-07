4bf40a9f10a48144e23da87c67f16295ce65c224
David.McGregor <David.McGregor@hl.co.uk>
2023-08-07 14:55:27 +0100

    added ProductManagerWeek1 page

--- src/Pages/ProductManagerPage.js
+++ src/Pages/ProductManagerPage.js
@@ -1,11 +1,104 @@
 import React from "react";
+import "./ProductManagerPage.css";
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import ProductManagerWeek1Div from "../ProductManagerPageComponents/ProductManagerWeek1Div";
+import ProductManagerFirstTwoWeeks from "../ProductManagerPageComponents/ProductManagerFirstTwoWeeks";
+import ProductManagerFirstMonth from "../ProductManagerPageComponents/ProductManagerFirstMonth";
 
 function ProductMangerPage() {
-    return (
-        <div className="ProductMangerPage">
-        <h1>ProductMangerPage</h1>
-        </div>
+  const [activeDiv, setActiveDiv] = useState("div1");
+  const divRefs = useRef([]);
+
+  useEffect(() => {
+    if (activeDiv === "div1") {
+      document.getElementById("mainHeader").textContent = "Week 1";
+    } else if (activeDiv === "div2") {
+      document.getElementById("mainHeader").textContent = "First two weeks";
+    } else if (activeDiv === "div3") {
+      document.getElementById("mainHeader").textContent = "First month";
+    }
+  }, [activeDiv]);
+
+  const goForward = () => {
+    const currentDiv = divRefs.current.find(
+      (ref) => ref.style.display !== "none"
     );
+    const currentIndex = divRefs.current.indexOf(currentDiv);
+    const nextIndex = (currentIndex + 1) % divRefs.current.length;
+
+    console.log("CurrentIndex  ->  " + currentIndex);
+    console.log("nextIndex  ->  " + nextIndex);
+
+    currentDiv.style.display = "none";
+    divRefs.current[nextIndex].style.display = "block";
+
+    setActiveDiv(`div${nextIndex + 1}`);
+  };
+
+  const goBackward = () => {
+    const currentDiv = divRefs.current.find(
+      (ref) => ref.style.display !== "none"
+    );
+    const currentIndex = divRefs.current.indexOf(currentDiv);
+    const previousIndex =
+      currentIndex === 0 ? divRefs.current.length - 1 : currentIndex - 1;
+
+    currentDiv.style.display = "none";
+    divRefs.current[previousIndex].style.display = "block";
+
+    setActiveDiv(`div${previousIndex + 1}`);
+  };
+
+  return (
+    <div className="mainDiv">
+      <div id="header">
+        <p className="arrow" data-testid="left-arrow" onClick={goBackward}>
+          &lt;
+        </p>
+        <h1 id="mainHeader">Week 1</h1>
+
+        <p className="arrow" data-testid="right-arrow" onClick={goForward}>
+          &gt;
+        </p>
+      </div>
+
+      <div className="flexDiv">
+        {/* <div className="arrowDivColumn">
+          <div className="leftArrowContainer">
+            <p className="arrow" onClick={goBackward}>
+              &lt;
+            </p>
+          </div>
+        </div> */}
+
+        <div ref={(el) => (divRefs.current[0] = el)}>
+          <ProductManagerWeek1Div />
+        </div>
+
+        <div
+          ref={(el) => (divRefs.current[1] = el)}
+          style={{ display: "none" }}>
+          <ProductManagerFirstTwoWeeks />
+        </div>
+
+        <div
+          ref={(el) => (divRefs.current[2] = el)}
+          style={{ display: "none" }}>
+          <ProductManagerFirstMonth />
+        </div>
+
+        <div className="arrowDivColumnR">
+          {/* <div className="rightArrowContainer">
+            <p className="arrow" onClick={goForward}>
+              &gt;
+            </p>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductMangerPage;
---
+++ src/ProductManagerPageComponents/ProductManagerFirstMonth.js
@@ -0,0 +1,160 @@
+import React from "react";
+import "./ProductManagerFirstMonth.css";
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import BigNames from "../UniversalComponents/BigNames";
+import Button from "../UniversalComponents/Button";
+import ResourcesComponent from "../UniversalComponents/ResourcesComponent";
+import GeneralTasksSection from "../UniversalComponents/GeneralTasksSection";
+
+const FirstMonth = () => {
+  const [
+    firstMonthReflectionPointsCollection,
+    setfirstMonthReflectionPointsCollection,
+  ] = useState([""]);
+
+  const [generalTasks, setGeneralTasks] = useState([
+    "Keep getting involved in group discussions",
+    "Complete a ticket independently",
+    "Fill out reflection points on any areas that could be improved.",
+    "Set up regular catch ups with your manager and buddy",
+    "Have a think about what you want to spend your training budget on",
+    "Attend a social event!",
+  ]);
+
+  const [devFirstMonthAllResources, setDevFirstMonthAllResources] = useState([
+    {
+      text: "Contextual Org Chart",
+      link: "Fake Link",
+    },
+    {
+      text: "Village Halls",
+      link: "Fake Link",
+    },
+    {
+      text: "Active Savings Confluence Space",
+      link: "Fake Link",
+    },
+    {
+      text: "HL Savings Digital Library",
+      link: "Fake Link",
+    },
+    {
+      text: "Coding Standards Miro",
+      link: "Fake Link",
+    },
+    {
+      text: "SonarQube",
+      link: "Fake Link",
+    },
+    {
+      text: "Savings Cheat Sheet",
+      link: "Fake Link",
+    },
+    {
+      text: "DSApp Code Layers",
+      link: "Fake Link",
+    },
+  ]);
+
+  const [newTask, setNewTask] = React.useState("");
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== "") {
+      console.log("Inside if statement");
+      let updatedGeneralTasksList = [...generalTasks];
+      updatedGeneralTasksList.push(newTask);
+      setGeneralTasks(updatedGeneralTasksList);
+    }
+
+    setNewTask("");
+    console.log("Working");
+  };
+
+  const [newReflectionPoint, setNewReflectionPoint] = React.useState("");
+
+  const handleAddReflectionPoint = () => {
+    if (newReflectionPoint.trim() !== "") {
+      let updatedfirstMonthReflectionPointsCollection = [
+        ...firstMonthReflectionPointsCollection,
+      ];
+      updatedfirstMonthReflectionPointsCollection.push(newReflectionPoint);
+      console.log(
+        "First Month Reflection Collection:  -> " +
+          updatedfirstMonthReflectionPointsCollection
+      );
+      setfirstMonthReflectionPointsCollection(
+        updatedfirstMonthReflectionPointsCollection
+      );
+    }
+
+    setNewReflectionPoint("");
+    alert("Submitted Reflection Point");
+  };
+
+  return (
+    <div id="TesterFirstMonthMainContainer">
+      {/* ---------- GENERAL TASKS ----------  */}
+
+      <GeneralTasksSection generalTasks={generalTasks} />
+
+      {/* ---------- RESOURCES ----------  */}
+
+      <ResourcesComponent
+        resources={devFirstMonthAllResources}
+        varient="FirstMonthDevResources"
+        mainDiv="DevFirstMonthMainResourceDiv"
+        resourceItem="DevFirstMonthResourceItem"
+      />
+
+      {/* ---------- REFLECTION POINTS ----------  */}
+
+      <div className="TesterFirstMonthReflectionPoints">
+        <h3 className="sectionHeader" id="reflectionPointsHeader">
+          Reflection Points
+        </h3>
+
+        <div className="addReflectionPointInputDiv">
+          <textarea
+            id="FirstMonthAddReflectionPointInputBox"
+            type="text"
+            value={newReflectionPoint}
+            onChange={(e) => setNewReflectionPoint(e.target.value)}
+          />
+        </div>
+
+        <div className="FirstMonthSubmitReflectionButtonDiv">
+          <Button label="Submit" onClick={handleAddReflectionPoint} />
+        </div>
+      </div>
+
+      {/* ---------- ADD TASK ----------  */}
+
+      <div className="TesterFirstMonthAddTask">
+        <h3 className="sectionHeader" id="addTaskHeader">
+          Add Task
+        </h3>
+
+        <div className="addTaskInputDiv">
+          <input
+            id="devFirstMonthAddTaskInputBox"
+            type="input"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+        </div>
+
+        <div className="FirstMonthAddTaskButtonDiv">
+          <Button
+            id="FirstMonthAddTaskButton"
+            label="Add Task"
+            onClick={handleAddTask}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FirstMonth;
